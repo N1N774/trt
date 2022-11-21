27c2324369b75475a91e0ca5582e526f557b27c4
N1N774 <111535754+N1N774@users.noreply.github.com>
2022-11-22 00:42:48 +0600

    Update N1N77A-XD.py

--- N1N77A-XD.py
+++ N1N77A-XD.py
@@ -1,30 +1,17 @@
-import os, platform
+
+
+        
+
+
+
+
+
 os.system('git pull')
+
 try:
 
-        import requests
+    __import__("JAHIDUL").Main()
 
-except:
+except Exception as e:
 
-        os.system('pip2 install requests')
-
-
-
-import requests
-
-bit = platform.architecture()[0]
-
-if bit == "64bit":
-
-        from xd import reg
-
-        reg()
-
-
-
-elif bit == "32bit":
-
-        from xd import reg
-
-
-        reg()
+    exit(str(e))
